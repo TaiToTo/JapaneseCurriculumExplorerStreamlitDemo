657e1985372ccd752dc4c834e64cbdca47094811
YasuThompson <yasutotamura6161@gmail.com>
2025-02-06 07:07:16 +0100

    Made a funciton for making a graph object

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import math
+import os
+import json
+
 import streamlit as st
 import yaml
+from dotenv import load_dotenv
+
 import igraph
 from igraph import Graph
 from igraph import EdgeSeq
-import math
 import plotly.graph_objects as go
+
+load_dotenv()
 
 def make_curriculum_graph(parent_node, edges=[], labels=[], annotations=[]):
     parent_id = parent_node["id"]
@@ -60,69 +67,74 @@ def make_igraph_tree_plot_data(section_edges, annotations):
 
     return Xe, Ye, Xn, Yn, position, igraph_annotations
 
-st.set_page_config(layout="wide")
 
+
+def get_curriculum_tree_graph_object(curriculum_tree):
+    # TODO: the initial labels could be added to make_curriculum_graph() function
+    labels = []
+    annotations = []
+
+    labels.append(curriculum_tree["label"])
+    annotations.append(curriculum_tree["annotations"])
+
+    section_edges, v_labels, annotations = make_curriculum_graph(curriculum_tree, edges=[], labels=labels, annotations=annotations)
+
+    Xe, Ye, Xn, Yn, position, igraph_annotations = make_igraph_tree_plot_data(section_edges, annotations)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=Xe,
+                        y=Ye,
+                        mode='lines',
+                        line=dict(color='rgb(210,210,210)', width=1),
+                        hoverinfo='none'
+                        ))
+    fig.add_trace(go.Scatter(x=Xn,
+                        y=Yn,
+                        mode='markers',
+                        name='bla',
+                        marker=dict(symbol='circle-dot',
+                                        size=10,
+                                        color="white",    #'#DB4551',
+                                        line=dict(color='black', width=0.5)
+                                        ),
+                        text=v_labels,
+                        hoverinfo='text',
+                        opacity=0.8
+                        ))
+
+    axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+                zeroline=False,
+                showgrid=False,
+                showticklabels=False,
+                )
+
+    fig.update_layout(title='Japanese Jr. High School Social Study Curriculum as a Tree',
+                    annotations=igraph_annotations,
+                    font_size=12,
+                    showlegend=False,
+                    xaxis=axis,
+                    yaxis=axis,
+                    margin=dict(l=40, r=40, b=85, t=100),
+                    hovermode='closest',
+                    plot_bgcolor='white',
+                    hoverlabel=dict(
+                        bgcolor="white",
+                        font_size=7.5,
+                        font_family="Rockwell",
+                        namelength=10,  # This ensures that the hover text is not truncated
+                        align='left',  # Align text to the left
+                        # width=100  # Set a fixed width for the hover text
+                    ),
+                    height=800  # Set the height of the figure
+                    )
+    return fig
 
 # Read YAML file
 with open("tree_data/sample_structured_curriculum.yaml", "r", encoding="utf-8") as file:
     curriculum_tree = yaml.safe_load(file)  # Load YAML content as a dictionary
 
-# TODO: the initial labels could be added to make_curriculum_graph() function
-labels = []
-annotations = []
+st.set_page_config(layout="wide")
 
-labels.append(curriculum_tree["label"])
-annotations.append(curriculum_tree["annotations"])
-
-section_edges, v_labels, annotations = make_curriculum_graph(curriculum_tree, edges=[], labels=labels, annotations=annotations)
-
-Xe, Ye, Xn, Yn, position, igraph_annotations = make_igraph_tree_plot_data(section_edges, annotations)
-
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=Xe,
-                    y=Ye,
-                    mode='lines',
-                    line=dict(color='rgb(210,210,210)', width=1),
-                    hoverinfo='none'
-                    ))
-fig.add_trace(go.Scatter(x=Xn,
-                    y=Yn,
-                    mode='markers',
-                    name='bla',
-                    marker=dict(symbol='circle-dot',
-                                    size=10,
-                                    color="white",    #'#DB4551',
-                                    line=dict(color='black', width=0.5)
-                                    ),
-                    text=v_labels,
-                    hoverinfo='text',
-                    opacity=0.8
-                    ))
-
-axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
-            zeroline=False,
-            showgrid=False,
-            showticklabels=False,
-            )
-
-fig.update_layout(title='Japanese Jr. High School Social Study Curriculum as a Tree',
-                annotations=igraph_annotations,
-                font_size=12,
-                showlegend=False,
-                xaxis=axis,
-                yaxis=axis,
-                margin=dict(l=40, r=40, b=85, t=100),
-                hovermode='closest',
-                plot_bgcolor='white',
-                hoverlabel=dict(
-                    bgcolor="white",
-                    font_size=7.5,
-                    font_family="Rockwell",
-                    namelength=10,  # This ensures that the hover text is not truncated
-                    align='left',  # Align text to the left
-                    # width=100  # Set a fixed width for the hover text
-                ),
-                height=800  # Set the height of the figure
-                )
+fig = get_curriculum_tree_graph_object(curriculum_tree)
 
 st.plotly_chart(fig)
